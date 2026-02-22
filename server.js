2fe2958a6f4f228206f6a4eb91e20c0192024c5f
mohammed sherif <mohamedsherif2612@gmail.com>
2026-02-23 01:03:11 +0200

    cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL ,
+    origin: ["https://ieee-obour.vercel.app","http://localhost:5173"] ,
     credentials: true
 }));
 app.use(express.json());
